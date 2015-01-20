ddddd3fe89c27261a8c92d056f2539b85f3182e8
Maxim Konakov <maxim2266@outlook.com>
2015-01-20 17:39:37 +0000

    Licence and documentation update.

--- parser/splitter.c
+++ parser/splitter.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013, Maxim Konakov
+Copyright (c) 2013, 2014, 2015, Maxim Konakov
  All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
